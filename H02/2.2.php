20b9f98e95955ecb99d6ac7f48af7f5d9f0a3552
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-09 13:40:38 +0200

    h2 almost finished

---
+++ H02/2.2.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+$volledigeZin = "Mensen ". $a. "en maken, werken ". $c;
+echo $volledigeZin;
+
---
+++ H02/2.4.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+$volledigeZin = "De cursus ". $a. " ". $b. " ". $c. "rstellingen\"  kan vanavond helaas niet doorgaan." ;
+echo $volledigeZin;
