569e52b35a3b9e25247608e7f8ccbb5a0161bc82
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-15 09:25:51 +0200

    small loop finished

---
+++ H03/opdracht 3.5.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>verandering</title>
+</head>
+<body>
+<?php
+//$x = 35;
+//while($x >= 25) {
+//    echo "hoppelepee";
+//    $x--;
+//
+//}
+
+for ($y = 35; $y > 25; $y--){
+
+    echo "hoppelepee";
+
+
+}
+?>
+
+</body>
+</html>
