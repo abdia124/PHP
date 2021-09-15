718a5b7a7243ffb88d09f26d33e187b91a94ae5e
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-15 15:21:04 +0200

    assignment with foreach start

---
+++ H03/opdracht 3.6.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>zwemmen</title>
+</head>
+<body>
+<?php
+
+$De_spartelkuikens = 25;
+$De_waterbuffels = 32;
+$Plonsmderin = 11;
+$Bommetje = 23;
+
+foreach ()
+?>
+</body>
+</html>
