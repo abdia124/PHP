3545242c15647fecbf0cc6690f2590bf8dba2c71
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-17 10:34:21 +0200

    almost finished for each loop H3

--- H03/opdracht 3.6.php
+++ H03/opdracht 3.6.php
@@ -10,23 +10,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>zwemmen</title>
+    <style>
+        img{
+            width: 40px;
+        }
+    </style>
 </head>
 <body>
 <?php
 
 $zwemmers = array("25", "32", "11", "23");
 
-//$plekken = array("De spartelkuikens", "De_waterbuffels", "Plonsmderin", "Bommetje" )
-//3;
+$plekken = array("De spartelkuikens", "De_waterbuffels", "Plonsmderin", "Bommetje" );
 
+foreach ($plekken as $plek){
 foreach ($zwemmers as $zwemmer) {
-    echo "de Spartelkuikens hebben $zwemmer zwemmers"; //$zwemmers[0];
-    for ($i = 5; $i < $zwemmer; $i = $i + 5) {
+    echo "$plek hebben $zwemmer zwemmers"; //$zwemmers[0];
+    for ($i = 4; $i < $zwemmer; $i = $i + 5) {
 
         echo "<img src= 'zwem.jpg'>";
 
     }
-}
+}}
 ?>
 </body>
 </html>
