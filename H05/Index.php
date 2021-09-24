b7f5cff849d59238b33de0ac0cae839e8f3b2a27
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-24 12:19:38 +0200

    first dynamic website complete

--- H05/Index.php
+++ H05/Index.php
@@ -11,10 +11,10 @@
     <title>Dynamic</title>
 </head>
 <body>
-<form action=”resultaat.php” method=”GET”>
-    Inlognaam <input type=”text” name=”inlognaam” value=””>
-    Wachtwoord <input type=”password” name=”wachtwoord” value=””>
-    <input type=”submit” name=”knop” value=”verstuur”>
+<form action="resultaat.php" method="GET">
+    Inlognaam <input type="text" name="inlognaam" value="">
+    Wachtwoord <input type="password" name="wachtwoord" value="">
+    <input type="submit" name="knop" value="verstuur">
 </form>
 
 </body>
