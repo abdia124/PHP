a5293560313af63e47fd50a8ef131165f6062cb1
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-09-27 13:53:52 +0200

    completed dynamic website

--- H05/Index.php
+++ H05/Index.php
@@ -19,13 +19,9 @@
 <body>
 <form action="resultaat.php" method="post">
     Voornaam <input type="text" name="Voornaam" value=""> <br> <br>
-    Tussenvoegsls <input type="text" name="Tussenvoegsls" value=""> <br> <br>
-    Achternaam <input type="text" name="Achternaam" value=""> <br> <br>
     E-mailadres <input type="email" name="E-mailadres" value=""><br><br>
-    Telefoon-nummer <input type="tel" name="Telefoon-nummer" value=""> <br> <br>
     Wachtwoord <input type="password" name="Wachtwoord" value="" > <br> <br>
-    Straatnaam <input type="text" name="Straatnaam" value=""> <br> <br>
-    Postcode & Huisnummer <input type="text" name="Postcode & Huisnummer" value=""> <br> <br>
+    Adres<input type="text" name="Adres" value=""> <br> <br>
     <input type="submit" name="knop" value="verstuur" >
 </form>
 
--- H05/resultaat.php
+++ H05/resultaat.php
@@ -1,4 +1,23 @@
 <?php
 if ($_POST['Voornaam'] == ""){
     echo "Naam vereisd";
+    echo "<a href= \"Index.php\"><br> Terug naar hoofdpagina</a> <br>";
+    
+}
+
+if ($_POST['E-mailadres'] == ""){
+    echo "Email vereisd";
+    echo "<a href= \"Index.php\"><br> Terug naar hoofdpagina</a> <br>";
+
+}
+if ($_POST['Adres'] == ""){
+    echo "Adres vereisd";
+    echo "<a href= \"Index.php\"><br> Terug naar hoofdpagina</a> <br>";
+
+}
+
+if ($_POST['Wachtwoord'] == ""){
+    echo "Wachtwoord vereisd";
+    echo "<a href= \"Index.php\"><br> Terug naar hoofdpagina</a> <br>";
+
 }
