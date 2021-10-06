24b645c466fdd004cd54c6652146ee6719b79fb9
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-10-06 14:44:52 +0200

    connectie met database gelegd

--- H06/Index.php
+++ H06/Index.php
@@ -10,7 +10,7 @@
     <title>SQL en PHP</title>
 </head>
 <body>
-
+<table>
 <?php
 $servername = "localhost";
 $dbname = "phpschool";
@@ -19,13 +19,32 @@ $password = "";
 
 try {
     $conn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-?>
+$query = "SELECT * FROM cursist";
+$stmt = $conn->prepare($query) or die("ERROR 1.");
+$stmt->execute() or die("ERROR 2.");
+while ($row = $stmt->fetch()){
+    echo "<tr>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
+    echo "<td>" .  $row["cursistnr"]."</td>";
 
+    echo "</tr>";
+
+
+}
+
+
+?>
+</table>
 </body>
 </html>
