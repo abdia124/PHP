4ca0ce6e0034104ab46d3428a70339325a09976e
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-10-08 10:48:31 +0200

    started with 2 login forms

---
+++ H07/index.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form name="login" class="form"  method="post">
+    E-mailadres <input type="email" name="E-mailadres" value=""><br><br>
+    Wachtwoord <input type="password" name="Wachtwoord" value=""> <br> <br>
+    <input type="submit" name="submit" value="login">
+</form>
+<form name="login" class="form"  method="post">
+    E-mailadres <input type="email" name="E-mailadres" value=""><br><br>
+    Wachtwoord <input type="password" name="Wachtwoord" value=""> <br> <br>
+    <input type="submit" name="submit" value="login">
+</form>
+</body>
+</html>
