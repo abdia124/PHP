e23da179736fe056ecd157da3a71b2bc05d2728a
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-10-11 14:39:46 +0200

    H7 continued

--- H07/index.php
+++ H07/index.php
@@ -29,15 +29,13 @@ if (isset($_POST['knop'])
 </head>
 <body>
 <h1> <?php echo $message; ?></h1>
-<form action="<?php echo $_SERVER['PHP_SELF'];?>" class="form" method="post">
-    E-mailadres <input type="email" name="E-mailadres" value=""><br><br>
-    Wachtwoord <input type="password" name="Wachtwoord" value=""> <br> <br>
-    <input type="submit" name="submit" value="login">
-</form>
-<form name="login" class="form" method="post">
-    E-mailadres <input type="email" name="E-mailadres" value=""><br><br>
-    Wachtwoord <input type="password" name="Wachtwoord" value=""> <br> <br>
-    <input type="submit" name="submit" value="login">
-</form>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+    <div class="form-group">
+        <label for="login">Login:</label>
+        <input type="text" name="login" value="">
+    </div>
+    <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" name="pwd" value="">
 </body>
 </html>
